8fc7a40ba61aa6dce5f3c07dd9810d0c04b4d8e3
PhuongBui <phuong.bui@saigontechnology.com>
2018-11-27 15:39:37 +0700

    update ui

--- wp-config.php
+++ wp-config.php
@@ -80,6 +80,7 @@ $table_prefix  = 'wp_';
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define('WP_DEBUG', false);
+define('FS_METHOD', 'direct');
 
 /* Đó là tất cả thiết lập, ngưng sửa từ phần này trở xuống. Chúc bạn viết blog vui vẻ. */
 
